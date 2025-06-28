29a3f08c0c60b39135f7b83ecf10cc26e74e4ec3
yazeed bani issa <ybaniissa92@example.com>
2025-06-28 04:50:50 +0300

    sec commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TheWorld-of-vision-for-eyes/',  // استبدل هذا باسم مستودع GitHub الخاص بك
+  base: '/-TheWorld-of-vision-for-eyes/',  // استبدل هذا باسم مستودع GitHub الخاص بك
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
